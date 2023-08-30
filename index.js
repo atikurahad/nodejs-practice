ab43f193597eb0a6707490bebfa7cdc79ceda57c
atikurahad <atikurahad@gmail.com>
2023-08-30 17:44:15 +0600

    make redirect

--- index.js
+++ index.js
@@ -19,19 +19,27 @@ app.get("/", function(req, res){
 app.get("/faq", function(req, res){
     res.send("Hello there, i am fror FAQ")
 })
-app.get("/about", function(req, res){
+app.patch("/about", function(req, res){
     res.send("I am from about")
 })
-app.get("/category", function(req, res){
+app.delete("/category", function(req, res){
     res.send("Hello there, i am from category")
 })
 app.get("/email", function(req, res){
     res.send("Hello there, i am from email")
 })
-app.get("/contact", function(req, res){
-    res.send("Hello there, i am from contact")
+app.put("/contact", function(req, res){
+    res.end("Hello there, i am from contact")
 })
 
-app.listen(8080,function(){
-    console.log("SERVER RUN SUCCESS")
+app.get("/redirect", function(req, res){
+
+    res.redirect('http://localhost:8080/email')
+
+})
+
+
+const PORT = 8080;
+app.listen(PORT,function(){
+    console.log(`SERVER RUN SUCCES ${PORT}`)
 })
